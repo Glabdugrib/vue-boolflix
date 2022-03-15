a064a9b1932c611b32c7fbb9f6c78823528587d1
Glabdugrib <si.sada93@gmail.com>
2022-03-15 19:07:14 +0100

    Milestone 2 completed

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
    <div id="app">
-      <input type="text" class="search-input" v-model="searchFilter" @keyup.enter="prova">
-      <button class="search-btn" @click="fetchMovies">Search</button>
+      <input type="text" class="search-input" v-model="searchFilter" @keyup.enter="searchTvMovies">
+      <button class="search-btn" @click="searchTvMovies">Search</button>
       <ul class="movie-list">
          <h3 v-if="filteredMovies.length > 0">Movie list</h3>
          <li v-for="movie in filteredMovies" :key="movie.id">
             <ul>
                <li><strong>Titolo: </strong>{{ movie.title }}</li>
-               <li><strong>Titolo originale: </strong>{{ movie.original_title }}</li>
-               <li><strong>Linguaggio: </strong>{{ movie.original_language }}</li>
-               <li><strong>Voto: </strong>{{ movie.vote_average }}</li>
+               <li><strong>Titolo originale: </strong>{{ movie.originalTitle }}</li>
+               <li v-if="movie.lang === 'it'">
+                  <strong>Linguaggio: </strong>
+                  <img src="./assets/img/italy.png" alt="">
+               </li>
+               <li v-else-if="movie.lang === 'fr'">
+                  <strong>Linguaggio: </strong>
+                  <img src="./assets/img/france.png" alt="">
+               </li>
+               <li v-else-if="movie.lang === 'en'">
+                  <strong>Linguaggio: </strong>
+                  <img src="./assets/img/united-kingdom.png" alt="">
+               </li>
+               <li v-else><strong>Linguaggio: </strong>{{ movie.lang }}</li>
+               <li><strong>Voto: </strong>{{ movie.vote }}</li>
             </ul>
          </li>
       </ul>
@@ -33,18 +45,61 @@ export default {
       }
    },
    methods: {
-      prova: function() {
-         console.log(this.searchFilter);
+      searchTvMovies: function() {
+         this.filteredMovies = [];
+         this.fetchMovies();
+         this.fetchTv();
+         
       },
       fetchMovies: function() {
          axios.get(`${ this.baseURL }/search/movie`, {
             params: {
                api_key: this.apiKey,
-               query: this.searchFilter
+               query: this.searchFilter,
+               // page: 1,
             }
          })
          .then( res => {
-            this.filteredMovies = res.data.results;
+            let movieList = [];
+            movieList = res.data.results;
+            console.log(movieList);
+
+            movieList.forEach( el => {
+               const movie = {
+                  title: el.title,
+                  originalTitle: el.original_title,
+                  lang: el.original_language,
+                  vote: el.vote_average
+               }
+               this.filteredMovies.push(movie);
+            });
+         })
+         .catch( err => {
+            console.warn( err.response )
+         })
+      },
+      fetchTv: function() {
+         axios.get(`${ this.baseURL }/search/tv`, {
+            params: {
+               api_key: this.apiKey,
+               query: this.searchFilter,
+               // page: 1,
+            }
+         })
+         .then( res => {
+            let tvList = [];
+            tvList = res.data.results;
+            console.log(tvList);
+
+            tvList.forEach( el => {
+               const tvSerie = {
+                  title: el.name,
+                  originalTitle: el.original_name,
+                  lang: el.original_language,
+                  vote: el.vote_average
+               }
+               this.filteredMovies.push(tvSerie);
+            });
          })
          .catch( err => {
             console.warn( err.response )
@@ -70,6 +125,11 @@ export default {
          list-style: none;
          padding: 0;
          margin: 0;
+
+         img {
+            aspect-ratio: 1;
+            width: 15px;
+         }
       }
    }
 }
